a95be51dc3d535d35d61d0821786cb4f33e2981a
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2014-10-16 17:43:16 +0300

    Make sure that if there is a thousands separator you remove it before turning that to integer

--- page_objects/pages/members_page.rb
+++ page_objects/pages/members_page.rb
@@ -1,13 +1,15 @@
 module PageObjects
   module Pages
     class MembersPage < SitePrism::Page
+      THOUSANDS_SEPARATOR = ','
+
       set_url "http://www.meetup.com{/meetup_name}/members/{?query*}"
 
       elements :message_links, 'div.memberActions a.j-composeNewMessage'
       element  :num_of_members, "div#C_document li.selected span.D_count"
 
       def number_of_members
-        num_of_members.text.gsub(/[()]/, '').to_i
+        num_of_members.text.gsub(/[()#{THOUSANDS_SEPARATOR}]/, '').to_i
       end
     end
   end
